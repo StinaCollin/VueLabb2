b65429fdd731ce4f1eef945c3ffdae81088baa54
Stina2022 <stina.collin@iths.se>
2023-02-09 16:42:06 +0100

    1st commit

---
+++ Vue-labb2/src/components/TheWelcome.vue
@@ -0,0 +1,63 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading></template>
+    <img src="../assets/img/bouquet_hero.jpg" alt="Hero" width="500">
+
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>Tooling</template>
+
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <EcosystemIcon />
+    </template>
+    <template #heading></template>
+
+
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <CommunityIcon />
+    </template>
+    <template #heading></template>
+
+
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <SupportIcon />
+    </template>
+    <template #heading>Kontakt</template>
+    Du är alltid välkommen att kontakta någon av oss som jobbar här:
+
+
+  </WelcomeItem>
+</template>
+<style>
+body {
+  background-image: url('../assets/img/bouquet_hero.jpg');
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
+}
+</style>
